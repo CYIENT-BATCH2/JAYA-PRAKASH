997ed23639a120a940a009cb1f67879e5a0df20d
jayaprakash207 <108973859+jayaprakash207@users.noreply.github.com>
2022-07-10 23:23:33 +0530

    Create DAY_1_PROB_2.c
    in above code, user have to give any of the  (a, e , i ,o, u )  characters in lower case 
    
    Q: Consider a character input from a user and print if it is a vowel or consonant.

---
+++ DAY1/DAY_1_PROB_2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+    
+    if ((c=='a')||(c=='e')||(c=='i')||(c=='o')||(c=='u'))
+    
+    {
+     printf(" vowels ");
+    }
+    else
+    {
+    printf("consonant");
+    }
+}
