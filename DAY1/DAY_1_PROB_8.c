9a76f167aa5abddd53aaec9627f1a77cef9089b1
jayaprakash207 <108973859+jayaprakash207@users.noreply.github.com>
2022-07-11 23:06:26 +0530

    Create DAY_1_PROB_8.c
    OUTPUT:
    Enter a character
    A
    The ASCII value of the character A is 65

---
+++ DAY1/DAY_1_PROB_8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+    char ch;
+    printf("Enter a character\n");
+    scanf("%c",&ch);
+    printf("The ASCII value of the character %c is %d",ch,ch);
+    return 0;
+}
