a786165156551de82b584d24caa4f377c890d57b
jayaprakash207 <108973859+jayaprakash207@users.noreply.github.com>
2022-07-11 22:38:02 +0530

    Create DAY_1_PROB_5.c
    OUTPUT:
    Assign the number for balls
    1 2 3 4 5 
    1 is odd
    2 is Even
    3 is odd
    4 is Even
    5 is odd

---
+++ DAY1/DAY_1_PROB_5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+  int b1,b2,b3,b4,b5;
+   printf("Assign the number for balls\n");
+   scanf("%d%d%d%d%d",&b1,&b2,&b3,&b4,&b5);
+   if(b1%2==0)
+       {
+       printf("%d is Even\n",b1);
+       }
+   else
+        printf("%d is odd\n",b1);
+   
+   if(b2%2==0)
+       {
+       printf("%d is Even\n",b2);
+       }
+       else
+       printf("%d is odd\n",b2);
+   if(b3%2==0)
+       {
+       printf("%d is Even\n",b3);
+       }
+       else
+       printf("%d is odd\n",b3);
+    if(b4%2==0)
+       {
+       printf("%d is Even\n",b4);
+       }
+       else
+       printf("%d is odd\n",b4);
+    if(b5%2==0)
+       {
+       printf("%d is Even\n",b5);
+       }
+       else
+       printf("%d is odd\n",b5);
+} 
