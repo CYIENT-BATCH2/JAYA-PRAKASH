281efc8c92773430f630de24f2b71d001976ac82
jayaprakash207 <108973859+jayaprakash207@users.noreply.github.com>
2022-07-12 23:26:34 +0530

    Create DAY_3_PROG_7.c

---
+++ DAY3/DAY_3_PROG_7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int y,m,m2;
+     printf("enter year and month ");
+scanf("%d",&y);
+scanf("%d",&m);
+if(y<=2022&&m>0&&m<=12)
+{
+    
+if((7-m)<0)
+y+=1;;
+m2=(12+7)-m;
+      printf("%d %d",2022-y,m2);
+}
+}
