a2b393c2608c7d588095ff656acca0ae09601c1d
jayaprakash207 <108973859+jayaprakash207@users.noreply.github.com>
2022-07-14 23:08:43 +0530

    Update DAY_2_PROB_1.c

--- DAY2/DAY_2_PROB_1.c
+++ DAY2/DAY_2_PROB_1.c
@@ -14,4 +14,26 @@ int main(){
 		goto label;
 	}
 	printf("sum=%ld",sum);
+	
 }
+
+/* //using for loop
+#include<stdio.h>
+int main(){
+	long int num , i;
+	long int rem,sum=0;
+	printf("enter the mobile number\n");
+	fflush(stdout);
+	scanf("%ld",&num);
+	
+	for (i=0;i<=num;i++)
+	{
+		rem=num%10;
+		sum=sum+rem;
+		num=num/10;
+	
+	}
+	printf("sum=%ld",sum);
+} */
+
+
